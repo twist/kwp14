405cdaa7866bf9d450b507a9fffb1dd3a330a7a0
bb <bb@twstmbl.de>
2013-11-03 00:05:12 +0100

    relationship testing and first basic fixtures

--- test/unit/topic_test.rb
+++ test/unit/topic_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class TopicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_relationships
+
+    topics =  Topic.all()
+    assert_not_nil topics.first.tags
+
+  end
 end
