65f9c533afb262ab5d586df4b89275d5d53cefe5
bb <bb@twstmbl.de>
2013-12-05 22:22:36 +0100

    cleaning

--- test/functional/votes_controller_test.rb
+++ test/functional/votes_controller_test.rb
@@ -5,45 +5,4 @@ class VotesControllerTest < ActionController::TestCase
     @vote = votes(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:votes)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create vote" do
-    assert_difference('Vote.count') do
-      post :create, vote: { topic_id: @vote.topic_id, user_id: @vote.user_id, value: @vote.value }
-    end
-
-    assert_redirected_to vote_path(assigns(:vote))
-  end
-
-  test "should show vote" do
-    get :show, id: @vote
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @vote
-    assert_response :success
-  end
-
-  test "should update vote" do
-    put :update, id: @vote, vote: { topic_id: @vote.topic_id, user_id: @vote.user_id, value: @vote.value }
-    assert_redirected_to vote_path(assigns(:vote))
-  end
-
-  test "should destroy vote" do
-    assert_difference('Vote.count', -1) do
-      delete :destroy, id: @vote
-    end
-
-    assert_redirected_to votes_path
-  end
 end
