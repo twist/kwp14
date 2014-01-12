1f35aea9936ff24aaec2d66322601a7c45903976
bb <bb@twstmbl.de>
2014-01-12 02:38:15 +0100

    filter and sort interface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,22 @@ require 'pp'
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :current_user
-    
+  helper_method :current_user, :filter_list, :sort_list
+
   private
+  def filter_list
+    @tags = Tag.all()
+    @filters = []
+    @tags.each do |t|
+      @filters.push t.title
+    end
+    return @filters
+  end
+
+  def sort_list
+    return ["Bewertung", "Nummer", "Neueste Kommentare"]
+  end
+    
       
   def current_user_session
     return @current_user_session if defined?(@current_user_session)
